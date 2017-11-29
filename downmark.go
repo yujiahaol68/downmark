44f60488444754a82f88859fedfe7fdb5e07fc17
Tecker.Yu <tecker_yuknigh@163.com>
2017-11-29 18:38:54 +0800

    concurrency process need to figure out

--- downmark.go
+++ downmark.go
@@ -2,15 +2,32 @@ package downmark
 
 import (
 	"fmt"
-	"io"
+	"net/http"
+	"net/url"
 	"strings"
+	"sync"
+	"time"
+
+	"golang.org/x/net/html"
 )
 
 // DLink save all the links
 type DLink []string
 
-// DBody is a concurrent safe map with key of link index and the res.Body of the link
-type DBody map[string]io.ReadCloser
+// DBody consist of index in the DLink
+type DBody struct {
+	Index      int
+	ParsedBody *html.Tokenizer
+}
+
+// Conversion is a concurrent safe map with key of link index and value of pointer of the converted string slice
+type Conversion map[string]*[]string
+
+const (
+	requestTimeOut = 5 * time.Second
+)
+
+var wg sync.WaitGroup
 
 // NewDLink produce a list contains link
 func NewDLink() DLink {
@@ -18,36 +35,78 @@ func NewDLink() DLink {
 	return d
 }
 
-func newDBody() DBody {
-	d := make(DBody)
+func newDBody(i int, t *html.Tokenizer) DBody {
+	return DBody{
+		Index:      i,
+		ParsedBody: t,
+	}
+}
+
+func newConversion() Conversion {
+	d := make(Conversion)
 	return d
 }
 
 // AddLink append link to the list
 func (d *DLink) AddLink(url string) error {
+	url = completeProtocol(strings.TrimSpace(url))
+
 	if isLink(url) {
-		*d = append(*d, completeProtocol(url))
+		*d = append(*d, url)
 		return nil
 	}
 	return fmt.Errorf("Expect URL but %v is not a url", url)
 }
 
 func isLink(l string) bool {
-	// use regex to deal it
+	_, err := url.ParseRequestURI(l)
+
+	if err != nil {
+		return false
+	}
 	return true
 }
 
 func completeProtocol(url string) string {
 	if strings.HasPrefix(url, "http") {
 		return url
+	} else if strings.HasPrefix(url, "://") {
+		return "https" + url
+	} else {
+		return "http://" + url
 	}
-	return "http://" + url
 }
 
-// GetBodys order go-routines to get the http body the concurrent
-func (d DLink) GetBodys() DBody {
-	dBo := newDBody()
+// ConvertToMarkDown order go-routines to get the http body the concurrently and convert them into markdown format
+func (d DLink) ConvertToMarkDown() (Conversion, error) {
+	if len(d) == 0 {
+		return nil, fmt.Errorf("Not URL can be used")
+	}
 
-	// go routine get the body and save in DBody map concurrent-safe way
-	return dBo
+	convertedBody := newConversion()
+	tokenizerChan := make(chan *DBody)
+
+	timeOut := time.Duration(requestTimeOut)
+	client := http.Client{
+		Timeout: timeOut,
+	}
+
+	for i, u := range d {
+		go func(id int, url string) {
+			resp, err := client.Get(url)
+
+			if err != nil {
+				fmt.Println(url + "FAIL in request")
+				return
+			}
+
+			defer resp.Body.Close()
+
+			dBody := newDBody(id, html.NewTokenizer(resp.Body))
+
+			tokenizerChan <- &dBody
+		}(i, u)
+	}
+
+	return convertedBody, nil
 }
--- downmark_test.go
+++ downmark_test.go
@@ -14,4 +14,9 @@ func Test_add_link(t *testing.T) {
 	if d[0] != "http://www.google.com" {
 		t.Errorf("Did not add http prefix")
 	}
+
+	err = d.AddLink("http//google.com")
+	if err == nil {
+		t.Errorf("should have error because 'http//google.com' is not a valid URL")
+	}
 }
